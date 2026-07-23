ed1c106a3dd0402d66b492074ca4cac60816993a
Tharuka Keerawella <dilshankeerawella@gmail.com>
2026-07-23 08:25:46 +0530

    Database configuartion

---
+++ lib/db_config.ts
@@ -0,0 +1,13 @@
+import mysql from "mysql2/promise";
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+});
+
+export default pool;
