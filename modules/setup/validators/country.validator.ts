0779218f9ee601bb84f2375a40911519712e1de2
Ishan200207 <168297757+Ishan200207@users.noreply.github.com>
2026-08-04 18:14:05 +0530

    feat: add country validation schema using Zod

---
+++ modules/setup/validators/country.validator.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+import { CountryStatus } from "@/constants/country.constants";
+
+// Country Validator
+export const CreateCountryValidator = z.object({
+    country_code: z.string().trim().min(2, "Country code is required!").max(5).transform(value => value.toUpperCase()),
+    country_name: z.string().trim().min(2, "Country name is required!").max(150),
+    country_status: z.enum([
+        CountryStatus.ACTIVE, CountryStatus.INACTIVE, CountryStatus.DELETED]).default(CountryStatus.ACTIVE)
+});
+
+export const UpdateCountrySchema = CreateCountryValidator.extend({
+    country_id: z.number()
+});
