236da5241ebc6d16839e83f1855ad49c5fce7057
Ishan200207 <168297757+Ishan200207@users.noreply.github.com>
2026-08-04 17:58:23 +0530

    feat: add country status constants

---
+++ constants/country.constants.ts
@@ -0,0 +1,8 @@
+// Country status constants
+export enum CountryStatus {
+    ACTIVE = "ACTIVE",
+    INACTIVE = "INACTIVE",
+    DELETED = "DELETED"
+}
+
+export const COUNTRY_STATUS_OPTIONS = Object.values(CountryStatus);
